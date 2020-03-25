3306ece20570c46a8d28042cd8b7c8af15016de5
nitiya <nita0018@stud.kea.dk>
2020-03-25 14:45:35 +0100

    nav done

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ $(function() {
     $(".nav-list").toggleClass("open");
   });
 
+  $(".nav-link").on("click", function() {
+    var act = $(".nav-list").hasClass("open");
+
+    if (act) {
+      $(".toggle").toggleClass("open");
+      $(".nav-list").toggleClass("open");
+    }
+  });
+
   AOS.init({
     easing: "ease",
     duration: 1000
